64fde641ae1f02653e20396a5e9bdb4294f93631
Kiritan <o1090549930o@outlook.com>
2023-06-24 14:14:03 +0800

    fixed

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import router from "./router";
 import groupList from "./data";
 import 'highlight.js/styles/stackoverflow-light.css'
 import 'highlight.js/lib/common';
+import hljsCommon from 'highlight.js/lib/common'
 import hljsVuePlugin from "@highlightjs/vue-plugin";
+
+hljsCommon.highlightAuto('<h1>Highlight.js has been registered successfully!</h1>').value
 
 createApp(App)
     .use(router)
